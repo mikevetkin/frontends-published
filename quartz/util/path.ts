8bbc4d94d4b589798badc7bb82710c90db2f8bc3
Michael <mikevetkin@gmail.com>
2024-02-28 03:16:01 +0300

    Quartz sync: Feb 28, 2024, 3:16 AM

--- quartz/util/path.ts
+++ quartz/util/path.ts
@@ -62,6 +62,90 @@ function sluggify(s: string): string {
     .replace(/\/$/, "")
 }
 
+// FIXME: Сделать по красоте
+function transliterate(word: string) {
+  var answer = "",
+    a: Record<string, unknown> = {}
+
+  a["Ё"] = "YO"
+  a["Й"] = "I"
+  a["Ц"] = "TS"
+  a["У"] = "U"
+  a["К"] = "K"
+  a["Е"] = "E"
+  a["Н"] = "N"
+  a["Г"] = "G"
+  a["Ш"] = "SH"
+  a["Щ"] = "SCH"
+  a["З"] = "Z"
+  a["Х"] = "H"
+  a["Ъ"] = "'"
+  a["ё"] = "yo"
+  a["й"] = "i"
+  a["ц"] = "ts"
+  a["у"] = "u"
+  a["к"] = "k"
+  a["е"] = "e"
+  a["н"] = "n"
+  a["г"] = "g"
+  a["ш"] = "sh"
+  a["щ"] = "sch"
+  a["з"] = "z"
+  a["х"] = "h"
+  a["ъ"] = "'"
+  a["Ф"] = "F"
+  a["Ы"] = "I"
+  a["В"] = "V"
+  a["А"] = "A"
+  a["П"] = "P"
+  a["Р"] = "R"
+  a["О"] = "O"
+  a["Л"] = "L"
+  a["Д"] = "D"
+  a["Ж"] = "ZH"
+  a["Э"] = "E"
+  a["ф"] = "f"
+  a["ы"] = "i"
+  a["в"] = "v"
+  a["а"] = "a"
+  a["п"] = "p"
+  a["р"] = "r"
+  a["о"] = "o"
+  a["л"] = "l"
+  a["д"] = "d"
+  a["ж"] = "zh"
+  a["э"] = "e"
+  a["Я"] = "Ya"
+  a["Ч"] = "CH"
+  a["С"] = "S"
+  a["М"] = "M"
+  a["И"] = "I"
+  a["Т"] = "T"
+  a["Ь"] = "'"
+  a["Б"] = "B"
+  a["Ю"] = "YU"
+  a["я"] = "ya"
+  a["ч"] = "ch"
+  a["с"] = "s"
+  a["м"] = "m"
+  a["и"] = "i"
+  a["т"] = "t"
+  a["ь"] = "'"
+  a["б"] = "b"
+  a["ю"] = "yu"
+
+  for (let i in word) {
+    if (word.hasOwnProperty(i)) {
+      if (a[word[i]] === undefined) {
+        answer += word[i]
+      } else {
+        answer += a[word[i]]
+      }
+    }
+  }
+  return answer
+}
+
 export function slugifyFilePath(fp: FilePath, excludeExt?: boolean): FullSlug {
   fp = stripSlashes(fp) as FilePath
   let ext = _getFileExtension(fp)
@@ -70,12 +154,14 @@ export function slugifyFilePath(fp: FilePath, excludeExt?: boolean): FullSlug {
     ext = ""
   }
 
-  let slug = sluggify(withoutFileExt)
+  let slug = transliterate(sluggify(withoutFileExt))
 
   // treat _index as index
   if (endsWith(slug, "_index")) {
     slug = slug.replace(/_index$/, "index")
   }
+
+  console.log("slug :>> ", slug)
 
   return (slug + ext) as FullSlug
 }
@@ -202,6 +288,9 @@ export interface TransformOptions {
 
 export function transformLink(src: FullSlug, target: string, opts: TransformOptions): RelativeURL {
   let targetSlug = transformInternalLink(target)
+
+  // FIXME: Здесь
+  console.log("targetSlug :>> ", targetSlug)
 
   if (opts.strategy === "relative") {
     return targetSlug as RelativeURL
