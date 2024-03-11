24a38746a12bc7370afb1a9a25441986667af7f8
Michael <mikevetkin@gmail.com>
2024-03-12 01:08:51 +0300

    Quartz sync: Mar 12, 2024, 1:08 AM

--- quartz/components/Backlinks.tsx
+++ quartz/components/Backlinks.tsx
@@ -12,6 +12,11 @@ const Backlinks: QuartzComponent = ({
 }: QuartzComponentProps) => {
   const slug = simplifySlug(fileData.slug!)
   const backlinkFiles = allFiles.filter((file) => file.links?.includes(slug))
+
+  if (backlinkFiles.length === 0) {
+    return null
+  }
+
   return (
     <div class={classNames(displayClass, "backlinks")}>
       <h3>{i18n(cfg.locale).components.backlinks.title}</h3>
